096c6b829a43011b368e5a16f03b656723763dfe
mlasch01 <mark.lasch@yahoo.com>
2024-08-25 17:05:06 -0500

    completed

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,38 @@
-const Table = ({ sat }) => {
-  return (
+import styling from "./styling.css";
+import satData from "./satData";
+
+  const Table = ({sat}) => {
+    return (
+    <>
+
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
-  );
+      <thead>
+        <tr>
+
+          <th scope="col">Name</th>
+          <th scope="col">Type of Satellite</th>
+          <th scope="col">Launch Date</th>
+          <th scope="col">Status</th>
+
+        </tr>
+      </thead>
+      <tbody>
+
+        {sat.map((data) => (
+          <tr key={data.id}>
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>{data.operational === true ? "Active" : "Inactive"}</td>
+
+          </tr>
+        ))};
+
+      </tbody>
+    </table>
+    
+   </>
+);
 };
 
 export default Table;
