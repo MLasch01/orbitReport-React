605986dbed9d57bcf7436dd0f6d7d2be6c881e45
mlasch01 <mark.lasch@yahoo.com>
2024-08-25 17:04:41 -0500

    completed

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,27 @@
+import styling from "./styling.css";
+import satData from "./satData";
+import App from "../App";
+import { useState } from "react";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className = "flex-container">
+      {displaySats.map((type, index) => {
+        return (
+
+          <button onClick={() => filterByType(type, setSat)} key={index}>
+            {type} Orbit
+          </button>
+        );
+      })}
+
+          <button onClick={() => setSat(satData)}>
+            All Orbits
+          </button>
     </div>
   );
 };
+
+
 
 export default Buttons;
